567022c52399e5f06ae9b766ba1a73be22dfeaa0
Wilmer Gaona <wgaonar@gmail.com>
2022-05-03 20:02:18 -0500

    Format changes

--- Section16/BaseClassPointers/main.cpp
+++ Section16/BaseClassPointers/main.cpp
@@ -8,51 +8,59 @@
 // We'll learn about virtual functions in the next video
 class Account {
 public:
-  virtual void withdraw(double amount) {
+  virtual void withdraw(double amount) 
+  {
     std::cout << "In Account::withdraw" << std::endl;
   }
   virtual ~Account() {  }
 };
 
-class Checking: public Account  {
+class Checking: public Account  
+{
 public:
-  virtual void withdraw(double amount) {
+  virtual void withdraw(double amount) 
+  {
     std::cout << "In Checking::withdraw" << std::endl;
   }
   
   virtual ~Checking() {  }
 };
 
-class Savings: public Account  {
+class Savings: public Account  
+{
 public:
-  virtual void withdraw(double amount) {
+  virtual void withdraw(double amount) 
+  {
     std::cout << "In Savings::withdraw" << std::endl;
   }
   virtual ~Savings() {  }
 };
 
-class Trust: public Account  {
+class Trust: public Account  
+{
 public:
-  virtual void withdraw(double amount) {
+  virtual void withdraw(double amount) 
+  {
     std::cout << "In Trust::withdraw" << std::endl;
   }
   virtual ~Trust() {  }
 };
 
-int main() {
-  std::cout << "\n === Pointers ==== " << std::endl;
+int main() 
+{
   Account *p1 = new Account();
   Account *p2 = new Savings();
   Account *p3 = new Checking();
   Account *p4 = new Trust();
   
+  std::cout << "\n === Pointers ==== " << std::endl;
   p1->withdraw(1000);
   p2->withdraw(1000);
   p3->withdraw(1000);
   p4->withdraw(1000);
   
   std::cout << "\n === Array ==== " << std::endl;
-  Account* array [] = {p1,p2,p3,p4};
+  Account* array[] = {p1,p2,p3,p4};
   for (auto i=0; i<4; ++i)
     array[i]->withdraw(1000);
       
