c0b1d1a5efc9b85a148f1a6bbdab883d9a7285e1
Wilmer Gaona <wgaonar@gmail.com>
2022-05-04 05:47:23 -0500

    Abstract base class

--- Section16/PureVirtualFunctions/main.cpp
+++ Section16/PureVirtualFunctions/main.cpp
@@ -3,26 +3,30 @@
 #include <iostream>
 #include <vector>
 
-class Shape {			// Abstract Base class
+class Shape // Abstract Base class
+{			
 private:
-       // attributes common to all shapes
+  // attributes common to all shapes
 public:
-      virtual void draw() = 0;	 // pure virtual function
-      virtual void rotate() = 0;   // pure virtual function
-      virtual ~Shape() { }
+  virtual void draw() = 0;	 // pure virtual function
+  virtual void rotate() = 0;   // pure virtual function
+  virtual ~Shape() { }
 };
 
-class Open_Shape: public Shape {    // Abstract class
+class Open_Shape: public Shape // Abstract class
+{    
 public:
-    virtual ~Open_Shape() { }
+  virtual ~Open_Shape() { }
 };
 
-class Closed_Shape : public Shape {  // Abstract class
+class Closed_Shape : public Shape // Abstract class
+{  
 public:
-    virtual ~Closed_Shape() { };
+  virtual ~Closed_Shape() { };
 };
 
-class Line: public Open_Shape {     // Concrete class
+class Line: public Open_Shape // Concrete class
+{     
 public:
     virtual void draw() override {
         std::cout << "Drawing a line" << std::endl;
@@ -33,25 +37,31 @@ public:
     virtual ~Line() {}
 };
 
-class Circle: public Closed_Shape {     // Concrete class
+class Circle: public Closed_Shape // Concrete class
+{     
 public:
-    virtual void draw() override {
-        std::cout << "Drawing a circle" << std::endl;
-    }
-    virtual void rotate() override {
-        std::cout << "Rotating a circle" << std::endl;
-    }
-    virtual ~Circle() {}
+  virtual void draw() override 
+  {
+    std::cout << "Drawing a circle" << std::endl;
+  }
+  virtual void rotate() override 
+  {
+    std::cout << "Rotating a circle" << std::endl;
+  }
+  virtual ~Circle() {}
 };
 
-class Square: public Closed_Shape {     // Concrete class
+class Square: public Closed_Shape // Concrete class
+{     
 public:
-    virtual void draw() override {
-        std::cout << "Drawing a square" << std::endl;
-    }
-    virtual void rotate() override {
-        std::cout << "Rotating a square" << std::endl;
-    }
+  virtual void draw() override 
+  {
+    std::cout << "Drawing a square" << std::endl;
+  }
+  virtual void rotate() override 
+  {
+    std::cout << "Rotating a square" << std::endl;
+  }
     virtual ~Square() {}
 };
 
@@ -62,16 +72,17 @@ void screen_refresh(const std::vector<Shape *> &shapes) {
         p->draw();
 }
 
-int main() {
-//    Shape s;
-//    Shape *p = new Shape();
+int main() 
+{
+  // Shape s;    // Error, Shape is the abstract base class
+  // Shape *p = new Shape(); // Error, Shape is the abstract base class
 
-//        Circle c;
-//        c.draw();
+  // Circle c;
+  // c.draw();
 
-//    Shape *ptr = new Circle();
-//    ptr->draw();
-//    ptr->rotate();
+  // Shape *ptr = new Circle();
+  // ptr->draw();
+  // ptr->rotate();
 
     Shape *s1 = new Circle();
     Shape *s2 = new Line();
