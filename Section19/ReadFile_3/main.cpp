421d46b34a7297cc34e7e5717bc654dcda6d2084
wgaonar <wgaonar@gmail.com>
2022-07-27 09:58:27 -0500

    Split the read line into words

--- Section19/ReadFile_3/main.cpp
+++ Section19/ReadFile_3/main.cpp
@@ -4,18 +4,28 @@
 #include <iostream>
 #include <fstream>
 
-int main() {
-    std::ifstream in_file;
-    in_file.open("../poem.txt");
-    if (!in_file) {
-        std::cerr << "Problem opening file" << std::endl;
-        return 1;
-    }
-    std::string line{};
-    while (std::getline(in_file, line)) {
-        std::cout << line << std::endl;
-    }
-    
-    in_file.close();
-    return 0;
+int main() 
+{
+  std::ifstream in_file;   // Declare the ifstream object
+
+  // Initialize the ifstream object and open it with the file to read
+  in_file.open("./poem.txt");
+
+  // Check if the file was open correctly
+  if (!in_file) 
+  {
+    std::cerr << "Problem opening file" << std::endl;
+    return 1;
+  }
+
+  std::string line{}; // The variable to store the lines of the file
+  
+  // Read continuously the file in a refactor way
+  while (std::getline(in_file, line)) 
+  {
+    std::cout << line << std::endl;
+  }
+  
+  in_file.close();
+  return 0;
 }
--- Section19/ReadFile_4/main.cpp
+++ Section19/ReadFile_4/main.cpp
@@ -1,22 +1,42 @@
 // Section 19
 // Read File 4
-// Read and display Shakespeare Sonnet 18 poem using unformatted get
+// Read and display word by word Shakespeare Sonnet 18 poem using getline
 #include <iostream>
 #include <fstream>
+#include <sstream> // To use istringstream objects to split into words a line
 
-int main() {
-    std::ifstream in_file;
-    in_file.open("../poem.txt");
-    if (!in_file) {
-        std::cerr << "Problem opening file" << std::endl;
-        return 1;
-    }
+int main() 
+{
+  std::ifstream in_file;   // Declare the ifstream object
 
-    char c {};
-    while (in_file.get(c)) {
-        std::cout << c;
+  // Initialize the ifstream object and open it with the file to read
+  in_file.open("./poem.txt");
+
+  // Check if the file was open correctly
+  if (!in_file) 
+  {
+    std::cerr << "Problem opening file" << std::endl;
+    return 1;
+  }
+
+  std::string line; // The variable to store the lines of the file
+  
+  // Read continuously the file in a refactor way
+  while (std::getline(in_file, line)) 
+  {    
+    std::cout << std::endl << line << std::endl; // Print the line
+
+    std::istringstream ss(line); // Object used to split the line around spaces.
+    std::string word;
+
+    // Traverse through all words in the line
+    while (ss >> word) 
+    {
+      // print the read word
+      std::cout << word << std::endl;
     }
-    std::cout << std::endl;
-    in_file.close();
-    return 0;
+  }
+  
+  in_file.close();
+  return 0;
 }
