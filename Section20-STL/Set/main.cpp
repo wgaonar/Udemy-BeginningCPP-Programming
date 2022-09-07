5f99524dbb28ba202f1d1ada6d1928b7a595374f
wgaonar <wgaonar@gmail.com>
2022-09-07 03:46:45 -0500

    Set added

--- Section20-STL/Set/main.cpp
+++ Section20-STL/Set/main.cpp
@@ -3,119 +3,156 @@
 #include <iostream>
 #include <set>
 
-class Person {
-    friend std::ostream &operator<<(std::ostream &os, const Person &p);
-    std::string name;
-    int age;
+class Person 
+{
+private:
+  // For overloading "<<" operator
+  friend std::ostream& operator<<(std::ostream &os, const Person &p);
+
+  std::string name;
+  int age;
 public:
-    Person() : name{"Unknown"}, age{0} {}
-    Person(std::string name, int age) 
-        : name{name}, age{age}  {}
-    bool operator<(const Person &rhs) const {
-        return this->age < rhs.age;
-    }
-    bool operator==(const Person &rhs) const {
-        return (this->name == rhs.name && this->age == rhs.age);
-    }
+  // Default constructor
+  Person() 
+    : name{"Unknown"}, age{0} 
+    {}
+
+  // Overload constructor
+  Person(std::string name, int age) 
+    : name{name}, age{age} 
+    {}
+
+  // Overload less "<" operator
+  // Sets uses it to check for the duplicate elements
+  bool operator<(const Person &rhs) const 
+  {
+    return this->age < rhs.age;
+  }
+
+  // Overload equal "==" operator
+  bool operator==(const Person &rhs) const 
+  {
+    return (this->name == rhs.name && this->age == rhs.age);
+  }
 };
 
-std::ostream &operator<<(std::ostream &os, const Person &p) {
-    os << p.name << ":" << p.age;
-    return os;
+// Friend function for overloading "<<" operator
+std::ostream &operator<<(std::ostream &os, const Person &p) 
+{
+  os << p.name << ":" << p.age;
+  return os;
 }
 
 template <typename T>
-void display(const std::set<T> &l) {
-    std::cout << "[ ";
-    for (const auto &elem: l) {
-        std::cout << elem << " ";
-    }
-    std::cout << "]" << std::endl;
+void display(const std::set<T> &s) 
+{
+  std::cout << "[ ";
+  for (const auto &elem: s) 
+  {
+    std::cout << elem << " ";
+  }
+  std::cout << "]" << std::endl;
 }
 
-void test1() {
-    // Sets
-    std::cout << "\nTest1 =========================" << std::endl;
-    std::set<int> s1 {1, 4, 3, 5, 2};
-    display(s1);
-    
-    s1 = {1,2,3,1,1,2,2,3,3,4,5};
-    display(s1);
-    
-    s1.insert(0);
-    s1.insert(10);
-    
-    display(s1);
-    
-    if (s1.count(10)) 
-        std::cout << "10 is in the set" << std::endl;
-    else
-        std::cout << "10 is NOT in the set" << std::endl;
+void test1() 
+{
+  // Sets
+  std::cout << "\nTest1 - display() .find() =========================" << std::endl;
+  std::set<int> s1 {1, 4, 3, 5, 2};
+  display(s1); // [ 1 2 3 4 5]
+  
+  s1 = {1,2,3,1,1,2,2,3,3,4,5};
+  display(s1); // [ 1 2 3 4 5 ]
+  
+  s1.insert(0);
+  s1.insert(10);  
+  display(s1); // [ 0 1 2 3 4 5 10 ]
+  
+  if (s1.count(10)) // 0 or 1 is the return of .count()
+  {
+    std::cout << "10 is in the set" << std::endl;
+  } 
+  else
+  {
+    std::cout << "10 is NOT in the set" << std::endl;
+  }
 
-    auto it = s1.find(5);
-    if (it != s1.end())
-        std::cout << "Found: " << *it << std::endl;    
-        
-    s1.clear();
-    
-    display(s1);
+  auto it = s1.find(5);
+  if (it != s1.end())
+  {
+    std::cout << "Found: " << *it << std::endl;    
+  }
+      
+  s1.clear();
+  
+  display(s1);
 }
 
-void test2() {
-    std::cout << "\nTest2 =========================" << std::endl;
-    std::set<Person> stooges {
-        {"Larry", 1},
-        {"Moe", 2},
-        {"Curly", 3}
-    };
-    display(stooges);           // Note the order of display!  operator<
+void test2() 
+{
+  std::cout << "\nTest2 - .emplace() .erase() =========================" << std::endl;
+  
+  std::set<Person> stooges 
+  {
+      {"Larry", 1},
+      {"Moe", 2},
+      {"Curly", 3}
+  };
+  display(stooges); // Note the order of display!  due to the operator "<"
 
-    stooges.emplace("James", 50);   
-    display(stooges);
+  stooges.emplace("James", 50);   
+  display(stooges);
 
-    stooges.emplace("Frank", 50); // NO -- 50 already exists
-    display(stooges);
-    
-    auto it = stooges.find(Person{"Moe", 2});
-    if (it != stooges.end())
-        stooges.erase(it);
-    
-    display(stooges);
-    
-    it = stooges.find(Person("XXXX", 50));      // Will remove James!!!!
-                                                                     // uses operator<
-    if (it != stooges.end())
-        stooges.erase(it);
-    display(stooges); 
+  stooges.emplace("Frank", 50); // NO -- 50 already exists
+  display(stooges);
+  
+  auto it = stooges.find(Person{"Moe", 2});
+  if (it != stooges.end())
+  {
+    stooges.erase(it);
+  }
+  
+  display(stooges);
+  
+  it = stooges.find(Person("XXXX", 50));     // Will remove James!!!!
+                                             // using the  operator "<"
+  if (it != stooges.end())
+  {
+    stooges.erase(it);
+  }
+  display(stooges); 
 }
 
-void test3() {
-    std::cout << "\nTest3 =========================" << std::endl;
-    
-    std::set<std::string> s {"A", "B", "C"};
-    display(s);
-    
-    auto result = s.insert("D");
-    display(s);
-    
-    std::cout << std::boolalpha;
-    std::cout << "first: " <<  *(result.first)<< std::endl;
-    std::cout  << "second: " << result.second << std::endl; 
-    
-    std::cout << std::endl;
+void test3() 
+{
+  std::cout << "\nTest3 =========================" << std::endl;
+  
+  std::set<std::string> s {"A", "B", "C"};
+  display(s);
+  
+  auto result = s.insert("D");  // Insert return std::pair <it, true/False>
+  display(s); // [ A B C D ]
+  
+  std::cout << std::boolalpha;
+  std::cout << "first: " <<  *(result.first)<< std::endl; // D
+  std::cout  << "second: " << result.second << std::endl; // true
+  
+  std::cout << std::endl;
 
-    result = s.insert("A");
-    display(s);
-    
-    std::cout << std::boolalpha;
-    std::cout << "first: " <<  *(result.first)<< std::endl;
-    std::cout  << "second: " << result.second << std::endl; 
+  result = s.insert("A");
+  display(s); // [ A B C D ] // A already exists!
+  
+  std::cout << std::boolalpha;
+  std::cout << "first: " <<  *(result.first)<< std::endl; // A
+  std::cout  << "second: " << result.second << std::endl; // false
 }
 
-int main() {
-    test1();
-    test2();
-    test3();
-    return 0;
+int main() 
+{
+  test1();
+  test2();
+  test3();
+  
+  return 0;
 }
 
