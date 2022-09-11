0029ed19b87cec7cea4b6ba1e2f5e46c952ff887
wgaonar <wgaonar@gmail.com>
2022-09-11 07:17:30 -0500

    Stack added

--- Section20-STL/Stack/main.cpp
+++ Section20-STL/Stack/main.cpp
@@ -7,48 +7,62 @@
 
 // This function displays a stack of 
 // by repeatedly topping and popping the stack
-// Note theat the stack is passed in by value so we
+// Note that the stack is passed in by value so we
 // don't affect the passed in stack.
 
 template <typename T>
-void display(std::stack<T> s) {
-    std::cout << "[ ";
-    while (!s.empty()) {
-        T elem = s.top();
-        s.pop();
-        std::cout << elem << " ";
-    }
-    std::cout << "]" << std::endl;
+void display(std::stack<T> s) 
+{
+  std::cout << "[ ";
+  while (!s.empty()) 
+  {
+    T elem = s.top();
+    s.pop();
+    std::cout << elem << " ";
+  }
+  std::cout << "]" << std::endl;
 }
 
-int main() {
-    std::stack<int> s;
-    std::stack<int, std::vector<int>> s1;
-    std::stack<int, std::list<int>> s2;
-    std::stack<int, std::deque<int>> s3;
-    
-    for (int i: {1,2,3,4,5}) 
-        s.push(i);
-    display(s);
+int main() 
+{
+  // Instantiate a stack from a deque by default
+  std::stack<int> s;
 
-    s.push(100);
-    display(s);
+  // Instantiate a stack from a vector
+  std::stack<int, std::vector<int>> s1;
+
+  // Instantiate a stack from a list
+  std::stack<int, std::list<int>> s2;
+
+  // Instantiate a stack explicitly from a deque
+  std::stack<int, std::deque<int>> s3;
     
+  for (int i: {1,2,3,4,5})
+  {
+    s.push(i);
+  }  
+  display(s); // [ 5 4 3 2 1 ]
+
+  s.push(100);
+  display(s); // [ 100 5 4 3 2 1 ]
+  
+  s.pop();
+  s.pop();
+  display(s); // [ 4 3 2 1 ]
+  
+  while (!s.empty())
+  {
     s.pop();
-    s.pop();
-    display(s);
-    
-    while (!s.empty())
-        s.pop();
-    display(s);
-    
-    std::cout << "Size: " << s.size() << std::endl;
-    
-    s.push(10);
-    display(s);
-    
-    s.top() = 100;
-    display(s);
-    return 0;
+  }
+  display(s); //[ ]
+  
+  std::cout << "Size: " << s.size() << std::endl; // Size: 0
+  
+  s.push(10); 
+  display(s); // [ 10 ] 
+  
+  s.top() = 100; // .top() is a reference and can be used to modified the top value
+  display(s); // [ 100 ]
+  return 0;
 }
 
