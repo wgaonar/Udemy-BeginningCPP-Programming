4f00798a097aadefa0d459aec3e3c20c3ee9f0cb
wgaonar <wgaonar@gmail.com>
2022-09-14 05:16:05 -0500

    Priority Queue added

--- Section20-STL/PriorityQueue/main.cpp
+++ Section20-STL/PriorityQueue/main.cpp
@@ -3,76 +3,96 @@
 #include <iostream>
 #include <queue>
 
-class Person {
-    friend std::ostream &operator<<(std::ostream &os, const Person &p);
-    std::string name;
-    int age;
+class Person 
+{
+  friend std::ostream &operator<<(std::ostream &os, const Person &p);
+  std::string name;
+  int age;
+
 public:
-    Person() : name{"Unknown"}, age{0} {}
-    Person(std::string name, int age) 
-        : name{name}, age{age}  {}
-    bool operator<(const Person &rhs) const {
-        // return this->age < rhs.age;
-        return this->name < rhs.name;
-    }
-    bool operator==(const Person &rhs) const {
-        return (this->name == rhs.name && this->age == rhs.age);
-    }
+  // Default constructor
+  Person() : name{"Unknown"}, age{0} {}
+
+  // Overload constructor
+  Person(std::string name, int age) 
+    : name{name}, age{age}  {}
+
+  // Overload less than "<" operator
+  bool operator<(const Person &rhs) const 
+  {
+    // return this->age < rhs.age; // If you desire order by age
+    return this->name < rhs.name;  // If you desire order by name
+  }
+
+  // Overload equal "=" operator
+  bool operator==(const Person &rhs) const 
+  {
+    return (this->name == rhs.name && this->age == rhs.age);
+  }
 };
 
-std::ostream &operator<<(std::ostream &os, const Person &p) {
-    os << p.name << ":" << p.age;
-    return os;
+// Friend function to print an object of the class Person
+std::ostream &operator<<(std::ostream &os, const Person &p) 
+{
+  os << p.name << ":" << p.age;
+  return os;
 }
+
 // This displays the priority queue by
 // repeatedly topping and popping the priority queue
 // It is being passed by value so we don't modify the 
 // priority queue being passed in
-
 template <typename T>
-void display(std::priority_queue<T> pq)  {
-    std::cout << "[ ";
-    while (!pq.empty()) {
-        T elem = pq.top();
-        pq.pop();
-        std::cout << elem << " ";
-    }
-    std::cout <<  "]" << std::endl;
-}
-
-void  test1() {
-    std::cout << "\nTest1 =========================" << std::endl;
-
-    std::priority_queue<int> pq;
-    for (int i : {3,5,7,12,23,12,4,100,0, 3,5,7})
-        pq.push(i);
-        
-    std::cout << "Size: " << pq.size() << std::endl;
-    std::cout << "Top: " << pq.top() << std::endl;
-        
-    display(pq);
-    
+void display(std::priority_queue<T> pq)  
+{
+  std::cout << "[ ";
+  while (!pq.empty()) 
+  {
+    T elem = pq.top();
     pq.pop();
-    display(pq);
+    std::cout << elem << " ";
+  }
+  std::cout <<  "]" << std::endl;
 }
 
-void test2() {
-    std::cout << "\nTest2 =========================" << std::endl;
+void  test1() 
+{
+  std::cout << "\nTest1 - .push() .pop() =========================" << std::endl;
 
-    std::priority_queue<Person> pq;
-    pq.push(Person{"A", 10});
-    pq.push(Person{"B", 1});
-    pq.push(Person{"C", 14});
-    pq.push(Person{"D", 18});  
-    pq.push(Person{"E", 7});  
-    pq.push(Person{"F", 27});
-    
-    display(pq);
+  std::priority_queue<int> pq;
+  for (int i : {3,5,7,12,23,12,4,100,0, 3,5,7})
+  {
+    pq.push(i);
+  }
+      
+  std::cout << "Size: " << pq.size() << std::endl;  // 12
+  std::cout << "Top: " << pq.top() << std::endl;    // 100
+      
+  display(pq); // [ 100 23 12 12 7 7 5 5 4 3 3 0 ]
+  
+  pq.pop();
+  display(pq); // [ 23 12 12 7 7 5 5 4 3 3 0 ]
 }
 
-int main() {
-    test1();
-    test2();
-    return 0;
+void test2() 
+{
+  std::cout << "\nTest2 - .push(Person)=========================" << std::endl;
+
+  std::priority_queue<Person> pq;
+  pq.push(Person{"A", 10});
+  pq.push(Person{"B", 1});
+  pq.push(Person{"C", 14});
+  pq.push(Person{"D", 18});  
+  pq.push(Person{"E", 7});  
+  pq.push(Person{"F", 27});
+  
+  display(pq); // [ F:27 E:7 D:18 C:14 B:1 A:10 ]
+}
+
+int main() 
+{
+  test1();
+  test2();
+  return 0;
 }
 
