00358a2802ab43b2a0e1094873597e41950b2461
wgaonar <wgaonar@gmail.com>
2022-09-18 05:34:53 -0500

    Output messages added

--- Section13-OOPClasesAndObjects/DelegatingConstructors/main.cpp
+++ Section13-OOPClasesAndObjects/DelegatingConstructors/main.cpp
@@ -19,19 +19,36 @@ public:
   Player(std::string name_val, int health_val, int xp_val);
 };
 
-Player::Player() : 
-  Player {"None", 0, 0} //Use the delegate constructor
-{cout << "No-args constructor" << endl;}
+// Default constructor
+Player::Player() : Player {"None", 0, 0} //Use the delegate constructor
+{
+  cout << "No-args constructor" << endl;
+  cout << "name: " << name << endl;
+  cout << "health: " << health << endl;
+  cout << "xp: " << xp << endl << endl;
+}
 
+// Overloaded constructor with one-arg
 Player::Player(std::string name_val) : 
   Player {name_val, 0, 0}  // Use the delegate constructor
-{cout << "One-arg constructor" << endl;}
+{
+  cout << "One-arg constructor" << endl;
+  cout << "name: " << name << endl;
+  cout << "health: " << health << endl;
+  cout << "xp: " << xp << endl << endl;
+}
   
+// Delegate constructor with all-args
 Player::Player(std::string name_val, int health_val, int xp_val) : 
   name {name_val}, 
   health {health_val}, 
   xp {xp_val} 
-{cout << "The delegate constructor with three-args" << endl;}
+{
+  cout << "The delegate constructor with three-args" << endl;
+  cout << "name: " << name << endl;
+  cout << "health: " << health << endl;
+  cout << "xp: " << xp << endl << endl;
+}
 
 int main() {
     
