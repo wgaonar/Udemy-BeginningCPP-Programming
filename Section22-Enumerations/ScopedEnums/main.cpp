b89d18ca0363558bb62ee9d961b53f29aa68a069
wgaonar <wgaonar@gmail.com>
2022-10-07 04:54:21 -0500

    Scope enums added

--- Section22-Enumerations/ScopedEnums/main.cpp
+++ Section22-Enumerations/ScopedEnums/main.cpp
@@ -4,78 +4,92 @@
 // Used for test1
 // Scoped enumeration holding the items for a grocery list
 // The value of the enumerator could be the item code
-enum class Grocery_Item {Milk=350, Bread=250, Apple=132, Orange=100};
+enum class Grocery_Item {Milk = 350, Bread = 250, Apple = 132, Orange = 100};
 
-// Used for test1
-// Overloaded stream insertion operator.
-// Inserts the name grocery item parameter to the input stream
+/*
+  Used for test1
+  Overloaded stream insertion operator.
+  Inserts the name grocery item parameter to the input stream
+*/
 std::ostream &operator<<(std::ostream &os, const Grocery_Item& grocery_item)
 {
-    std::underlying_type_t<Grocery_Item> value = std::underlying_type_t<Grocery_Item>(grocery_item);
-	switch (grocery_item) {
-	    case Grocery_Item::Milk:      
-            os << "Milk";
-            break;
+  // To avoid infinite recursion printing the items
+  std::underlying_type_t<Grocery_Item> value = std::underlying_type_t<Grocery_Item>(grocery_item);
+	
+  switch (grocery_item) 
+  {
+    case Grocery_Item::Milk:      
+      os << "Milk";
+      break;
 		case Grocery_Item::Bread:
-            os << "Bread";
-            break;
+      os << "Bread";
+      break;
 		case Grocery_Item::Apple:     
-            os << "Apple";
-            break;
+      os << "Apple";
+      break;
 		case Grocery_Item::Orange:    
-            os << "Orange";
-            break;
+      os << "Orange";
+      break;
 		default:        
-            os << "Invalid item";
+      os << "Invalid item";
 	}
-    os << " : " << value;
+  os << " : " << value;
 	return os;
 }
 
-// Used for test1
-// Returns a boolean depending on whether the Grocery_Item
-// paramter is a valid enumerator or not.
-bool is_valid_grocery_item(Grocery_Item grocery_item)
+/*
+  Used for test1
+  Returns a boolean depending on whether the Grocery_Item
+  parameter is a valid enumerator or not.
+*/
+bool is_valid_grocery_item(const Grocery_Item& grocery_item)
 {
-	switch (grocery_item) {
-	    case Grocery_Item::Milk:  
-        case Grocery_Item::Bread:
-        case Grocery_Item::Apple:
-        case Grocery_Item::Orange:
-            return true;
+	switch (grocery_item) 
+  {
+    case Grocery_Item::Milk:  
+    case Grocery_Item::Bread:
+    case Grocery_Item::Apple:
+    case Grocery_Item::Orange:
+      return true;
 		default:        
-            return false; 
+      return false; 
 	}
 }
 
-// Used for test1
-// Displays the grocery items in the grocery list
-// Given a vector of Grocery_Items, this function displays
-// the string representation of each item using the overloaded
-// operator<< function.
-// This function also keeps track of how many valid and invalid items
-// are in the provided vector by using the is_valid_grocery_item function
-// written above.
+/* 
+  Used for test1
+  Displays the grocery items in the grocery list
+  Given a vector of Grocery_Items, this function displays
+  the string representation of each item using the overloaded
+  operator<< function.
+  This function also keeps track of how many valid and invalid items
+  are in the provided vector by using the is_valid_grocery_item function
+  written above. 
+*/
 void display_grocery_list(const std::vector<Grocery_Item> &grocery_list)
 {
 	std::cout << "Grocery List" << "\n==============================" << std::endl;
-    int invalid_item_count{0};
-    int valid_item_count {0};
+  int invalid_item_count{0};
+  int valid_item_count {0};
 	for (Grocery_Item grocery_item : grocery_list)
 	{
 		std::cout << grocery_item << std::endl;  // User the overloaded operator<<
 		
 		// Check that grocery is valid grocery item
 		if (is_valid_grocery_item(grocery_item))
-            valid_item_count++;
-        else
+    {
+      valid_item_count++;
+    }
+    else
+    {
 			invalid_item_count++;
+    }
 	}
 	
 	std::cout << "==============================" << std::endl;
-    std::cout << "Valid items: " <<  valid_item_count << std::endl;
-    std::cout << "Invalid items: " <<  invalid_item_count << std::endl;
-	std::cout << "Total items: " <<  valid_item_count +invalid_item_count << std::endl;
+  std::cout << "Valid items: " <<  valid_item_count << std::endl;
+  std::cout << "Invalid items: " <<  invalid_item_count << std::endl;
+	std::cout << "Total items: " <<  valid_item_count + invalid_item_count << std::endl;
 }
 
 // Using a scoped enumeration to model grocery items
@@ -88,10 +102,14 @@ void test1() {
 	shopping_list.push_back(Grocery_Item::Milk);
 	shopping_list.push_back(Grocery_Item::Orange);
 	
-    int Helicopter {1000};
- // shopping_list.push_back(Helicopter);                // Compiler error
-	shopping_list.push_back(Grocery_Item(Helicopter));  // Invalid item
-    shopping_list.push_back(Grocery_Item(350));         // Will add Milk again!
+  // shopping_list.push_back(Helicopter);                // Compiler error
+  
+  int Helicopter {1000};
+	shopping_list.push_back(Grocery_Item(Helicopter));  // It is an invalid item but WITHOUT error
+  
+  // Add items using the numeric key through casting
+  shopping_list.push_back(Grocery_Item(350));         // Will add Milk again!
+  shopping_list.push_back(Grocery_Item(250));         // Will add Bread
 	
 	display_grocery_list(shopping_list);
 }
@@ -100,54 +118,78 @@ void test1() {
 // A simple class that might model a Player in a game application.
 // The player has a name, a mode that they are currently in, and a
 // direction which they are facing.
-class Player {
-    friend std::ostream &operator<<(std::ostream &os, const Player &p);
+class Player 
+{
+  friend std::ostream &operator<<(std::ostream &os, const Player &p);
+
 public:
-    enum class Mode {Attack, Defense, Idle};
-    enum class Direction {North, South, East, West};
-    
-    Player(std::string name, Mode mode = Mode::Idle, Direction direction = Direction::North):
-        name{name}, mode{mode}, direction{direction} {}
+  enum class Mode {Attack, Defense, Idle};
+  enum class Direction {North, South, East, West};
+  
+  // Delegate constructor
+  Player(std::string name = "none", Mode mode = Mode::Idle, Direction direction = Direction::North):
+    name{name}, mode{mode}, direction{direction} {}
 
-    std::string get_name() const {
-        return name;
-    }
-    void set_name(std::string name) {
-        this->name = name;
-    }
-    
-    Mode get_mode() const {return mode; }
-    void set_mode(Mode mode) {
-        this->mode = mode;
-    }
+  // Getters
+  std::string get_name() const 
+  {
+    return name;
+  }
 
-    Direction get_direction() const {return direction; }
-    void set_direction(Direction direction) {
-        this->direction = direction;
-    }
+  Mode get_mode() const 
+  {
+    return mode; 
+  }
+  
+  Direction get_direction() const 
+  {
+    return direction; 
+  }
+
+  // Setters
+  void set_name(std::string name) 
+  {
+    this->name = name;
+  }
+  
+  void set_mode(Mode mode) 
+  {
+    this->mode = mode;
+  }
+
+  void set_direction(Direction direction) 
+  {
+    this->direction = direction;
+  }
+
+// Coded in the last part, after the enums declaration as public    
 private:
-    std::string name;
-    Mode mode;
-    Direction direction;
+  std::string name;
+  Mode mode;
+  Direction direction;
 };
 
-// Used for test2
-// A simple function that returns the string representation
-// of the Player::Mode paramter passed into it.
-std::string get_player_mode(Player::Mode mode) {
-    std::string result;
-    switch (mode) {
-        case Player::Mode::Attack:
-            result = "Attack"; 
-            break;
-        case Player::Mode::Defense:
-            result = "Defense";
-            break;
-        case Player::Mode::Idle:
-            result = "Idle";
-            break;
-    };
-    return result;
+/* 
+  Used for test2
+  A simple function that returns the string representation
+  of the Player::Mode parameter passed into it. 
+*/
+std::string get_player_mode(Player::Mode mode) 
+{
+  std::string result;
+  switch (mode) 
+  {
+    case Player::Mode::Attack:
+      result = "Attack"; 
+      break;
+    case Player::Mode::Defense:
+      result = "Defense";
+      break;
+    case Player::Mode::Idle:
+      result = "Idle";
+      break;
+  };
+  return result;
 }
 
 // Used for test2
